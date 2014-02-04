8c1adca0499fcfe158085c71648fc42d82765d05
CerealGuy <kerle@kerle-MS-7504VP-PV.(none)>
2014-02-04 19:31:14 +0100

    completed hide-reply, settings, lang

---
+++ hide/lang/HideModAdmin.english.php
@@ -0,0 +1,9 @@
+<?php
+
+$txt['hidemod_sa1'] = "[hide] Locked:";
+$txt['hidemod_sa2'] = "[hide] unlocked before:";
+$txt['hidemod_sa3'] = "[hide] unlocked after:";
+$txt['hidemod_sb1'] = "[hide-reply] Locked:";
+$txt['hidemod_sb2'] = "[hide-reply] unlocked before:";
+$txt['hidemod_sb3'] = "[hide-reply] unlocked after:";
+?>
--- hide/src/HideMod.Main.php
+++ hide/src/HideMod.Main.php
@@ -2,20 +2,31 @@
 
 function hmDisplayPostDone(&$counter, &$output)
 {
-	global $user;
-	$pattern_search_hide = '/\[hide\](.*?)\[\/hide\]/';
-	if(allowed_to_see_hide($output)){
-		$output['body'] = preg_replace('/\[hide\]/', '<center>Hidden Content:</center><br>', $output['body']);
-		$output['body'] = preg_replace('/\[\/hide\]/', '', $output['body']);
+	global $user, $settings;
+	$pattern_search_hide = '/\[hide\](.*?)\[\/hide\]/'; //regex for hide
+	$pattern_search_hide_reply = '/\[hide-reply\](.*?)\[\/hide-reply\]/'; //regex for hide-reply
+
+	if(strpos($output['body'],'hide') == false) return; //check if post contains hide, if not skip this shit here
+	
+	if(allowed_to_see_hide($output)){ //check if user is allowed to see [hide]
+		//hes allowed to see it, therefore clean up bbcode and do before/after stuff
+		$output['body'] = preg_replace('/\[hide\]/', $settings['hidemod_sa2'], $output['body']); 
+		$output['body'] = preg_replace('/\[\/hide\]/', $settings['hidemod_sa3'], $output['body']);
 	}else{
-		$output['body'] = replace_bbcode($pattern_search_hide, $output['body'], "<center>Hidden Content<br>You need to like to see hidden content</center>");
+		//hes not allowed to see it, therefore replace [hide....]blubb[/hide] with a message
+		$output['body'] = replace_bbcode($pattern_search_hide, $output['body'], $settings['hidemod_sa1']);
 	}
 	//print_r($output);
-
+	if(allowed_to_see_hide_reply($output)){ //same like line 11 but checks for hide-reply
+		$output['body'] = preg_replace('/\[hide-reply\]/', $settings['hidemod_sb2'], $output['body']);
+		$output['body'] = preg_replace('/\[\/hide-reply\]/', $settings['hidemod_sb3'], $output['body']);
+	}else{
+		$output['body'] = replace_bbcode($pattern_search_hide_reply, $output['body'], $settings['hidemod_sb1']);
+	}
 
 }
 function allowed_to_see_hide($output){
-	if(allowed_to_see($output)){ return true;}
+	if(allowed_to_see($output)){ return true;} //check if user is owner/admin
 	$result = wesql::query('SELECT * FROM {db_prefix}likes WHERE id_content = {int:post_id} AND content_type = "post" AND id_member = {int:member_id}', 
 			array(
 				'member_id' => we::$user['mod_cache']['id'],
@@ -32,10 +43,10 @@ function allowed_to_see_hide($output){
 }
 
 function allowed_to_see_hide_reply($output){
-	if(allowed_to_see($output)){ return true;}
-
-
-	return false;
+	if(allowed_to_see($output)){ return true;} //check if user is owner/admin
+	$topicid = topic_id_from_post_id($output['id']);
+	
+	return user_has_replied_to_topic($topicid, we::$user['mod_cache']['id']);
 	
 }
 
@@ -64,4 +75,30 @@ function replace_bbcode($pattern, $body, $replace){
 
 }
 
+function topic_id_from_post_id($postid){
+	$query = wesql::query("SELECT id_topic
+				FROM {db_prefix}messages
+				WHERE id_msg = {int:post_id}
+				LIMIT 1 ", array('post_id' => $postid));
+
+	if(wesql::num_rows($query) > 0){
+		$row = wesql::fetch_assoc($query);
+		return $row['id_topic'];
+	}
+}	
+
+function user_has_replied_to_topic($topicid, $userid){
+	$query = wesql::query("SELECT * 
+				FROM {db_prefix}messages 
+				WHERE id_topic = {int:id_topic} 
+				and id_member = {int:id_user} LIMIT 1",	array(
+					'id_topic' => $topicid,
+					'id_user' => $userid));
+	if(wesql::num_rows($query) > 0){
+		return true;
+	}
+	return false;
+					
+
+}
 ?>
