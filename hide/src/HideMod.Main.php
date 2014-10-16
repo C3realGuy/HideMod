5d56b09dcfffa5c40d014ec24a98f1629312cd6b
CleptoMan <sendme@nomail.gov>
2014-10-16 10:46:16 +0200

    Could look threw hide via PrintPage, fixed but need more information on this from wedge.

--- hide/src/HideMod.Main.php
+++ hide/src/HideMod.Main.php
@@ -24,38 +24,47 @@ function hmQuoteFastDone(&$xml, $row){
 function hmPostBBCParse(&$message, &$bbc_options, &$type){
 	loadPluginSource('CerealGuy:HideMod', 'src/HideMod-Subs'); 
 	global $context, $settings, $pattern_search_bbc, $pattern_search_hide, $pattern_search_hide_reply, $topicinfo, $topic, $board;
+	log_error(print_r($bbc_options, true));
 	$allowed_types = array('post', 'post-preview'); //perhaps interesting for tweaking at some day
 	$disabled_boards = !empty($settings['hidemod_disabled_boards']) ? unserialize($settings['hidemod_disabled_boards']) : array(); // nice line, modified from TopicSolved, prepares board settings from acp
-
-	if(isset($bbc_options['cache']) and isset($topic) and in_array($type, $allowed_types)){
-
-		if($topicinfo['id_first_msg'] == $bbc_options['cache']){
-			// It's the first post of a topic, therefore we already have the id_member_started info
-			$god = allowed_to_see($topicinfo['id_member_started']);
-		}else{
-			// It's a post, but not the first, therefore we have to get that info
-			$god = allowed_to_see(get_post_author($bbc_options['cache']));
-			
-		}
-		if(in_array($board, $disabled_boards)){ // check if board is in disabled_boards (settings in acp)
-			$god = true;
-		}
-		if($god or user_has_liked_post($bbc_options['cache'])){
-			$message = preg_replace('/\[hide\]/', $settings['hidemod_sa2'], $message);
-
-			$message = preg_replace('/\[\/hide\]/', !empty($settings['hidemod_sa3']) ? $settings['hidemod_sa3'] : '', $message);
-		}else{
-			// not allowed hide
-			$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
-		}
-
-		if($god or user_has_replied_to_topic($topic)){
-			$message = preg_replace('/\[hide-reply\]/', $settings['hidemod_sb2'], $message);
-			$message = preg_replace('/\[\/hide-reply\]/', !empty($settings['hidemod_sb3']) ? $settings['hidemod_sb3'] : '', $message);
-		}else{
-			$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
-		}
+	if(!in_array($type, $allowed_types))
+		return;
+	
+	if(isset($bbc_options['print']) && $bbc_options['print'] == 1){
+		// We dont know post id, therefore censor always.
+		$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
+		$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
+		return;
 	}
+	$topic_id_first_msg = $topicinfo['id_first_msg'];
+	$topic_id_member_started = $topicinfo['id_member_started'];
+	$topic_id = $bbc_options['cache'];
+
+	if($topic_id_first_msg == $topic_id){
+		// It's the first post of a topic, therefore we already have the id_member_started info
+		$god = allowed_to_see($topic_id_member_started);
+	}else{
+		// It's a post, but not the first, therefore we have to get that info
+		$god = allowed_to_see(get_post_author($topic_id));
+			
+	}
+	if(in_array($board, $disabled_boards)){ // check if board is in disabled_boards (settings in acp)
+		$god = true;
+	}
+	if($god or user_has_liked_post($topic_id)){
+		$message = preg_replace('/\[hide\]/', $settings['hidemod_sa2'], $message);
+		$message = preg_replace('/\[\/hide\]/', !empty($settings['hidemod_sa3']) ? $settings['hidemod_sa3'] : '', $message);
+	}else{
+		// not allowed hide
+		$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
+	}
+	if($god or user_has_replied_to_topic($topic)){
+		$message = preg_replace('/\[hide-reply\]/', $settings['hidemod_sb2'], $message);
+		$message = preg_replace('/\[\/hide-reply\]/', !empty($settings['hidemod_sb3']) ? $settings['hidemod_sb3'] : '', $message);
+	}else{
+		$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
+	}
+
 	
 
 }
