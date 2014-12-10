8670a371ba7b60cc87c1e1c606c5cd576cc2d605
CleptoMan <sendme@nomail.gov>
2014-12-10 20:20:20 +0100

    now using mods.xml. Awesome work Nao! No need to add an "custom hook" anymore. Just plug and play.

--- README.md
+++ README.md
@@ -13,18 +13,7 @@ Installation:
 -------------
 
 Drop "hide" folder in your /wedgefolder/plugins and activate it in the adminpanel.
-At the moment you need to add an own hook into wedge because there's no one to handle
-quoting stuff (and if we dont handle it, its possible to look threw the hide when you
-quote^^)
-Therefore you need to make two small changes in your core files:
-
-
-In /core/app/ManagePlugins.php
-Search ``// Content creation`` and add afterwards ``'quote_fast_done',``
-
-In /core/app/QuoteFast.php
-Search ``return_xml('<we><quote>', cleanXml($xml), '</quote></we>');``
-and add before ``call_hook('quote_fast_done', array(&$xml, &$_REQUEST['quote'], &$row));``
+If you want you can change the default messages. Have a look at the next point.
 
 Example German Config:
 ----------------------
--- hide/src/HideMod.Main.php
+++ hide/src/HideMod.Main.php
@@ -3,14 +3,14 @@
 if (!defined('WEDGE'))
 	die('Hacking attempt...');
 
-function hmQuoteFastDone(&$xml, $row){
+function hmQuoteFastDone($xml, $row){
 	$post_id = $_REQUEST['quote'];
-	loadPluginSource('CerealGuy:HideMod', 'src/HideMod-Subs');
+	loadPluginSource('CerealGuy:HideMod', 'src/HideMod-Subs'); 
 	global $pattern_search_hide, $pattern_search_hide_reply, $settings;
 	print $row['id_member'];
 	if(we::$is['admin'] or MID == $row['id_member']){
 		//User is admin or 
-		return;
+		return $xml;
 	}
 	if(!user_has_replied_to_topic($row['id_topic'])){
 		$xml = preg_replace($pattern_search_hide_reply, $settings['hidemod_sc2'], $xml);
@@ -18,54 +18,46 @@ function hmQuoteFastDone(&$xml, $row){
 	if(!user_has_liked_post($post_id)){
 		$xml = preg_replace($pattern_search_hide, $settings['hidemod_sc1'], $xml);
         }
+	return $xml;
+	
 
 }
 
 function hmPostBBCParse(&$message, &$bbc_options, &$type){
 	loadPluginSource('CerealGuy:HideMod', 'src/HideMod-Subs'); 
 	global $context, $settings, $pattern_search_bbc, $pattern_search_hide, $pattern_search_hide_reply, $topicinfo, $topic, $board;
-
 	$allowed_types = array('post', 'post-preview'); //perhaps interesting for tweaking at some day
 	$disabled_boards = !empty($settings['hidemod_disabled_boards']) ? unserialize($settings['hidemod_disabled_boards']) : array(); // nice line, modified from TopicSolved, prepares board settings from acp
-	if(!in_array($type, $allowed_types))
-		return;
-	
-	if(isset($bbc_options['print']) && $bbc_options['print'] == 1 && empty($bbc_options['cache'])){
-		// We dont know post id, therefore censor always.
-		$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
-		$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
-		return;
-	}
-	$topic_id = $bbc_options['cache'];
-	$topic_id_first_msg = $topicinfo['id_first_msg'];
-	$topic_id_member_started = $topicinfo['id_member_started'];
 
+	if(isset($bbc_options['cache']) and isset($topic) and in_array($type, $allowed_types)){
 
-	if($topic_id_first_msg == $topic_id){
-		// It's the first post of a topic, therefore we already have the id_member_started info
-		$god = allowed_to_see($topic_id_member_started);
-	}else{
-		// It's a post, but not the first, therefore we have to get that info
-		$god = allowed_to_see(get_post_author($topic_id));
+		if($topicinfo['id_first_msg'] == $bbc_options['cache']){
+			// It's the first post of a topic, therefore we already have the id_member_started info
+			$god = allowed_to_see($topicinfo['id_member_started']);
+		}else{
+			// It's a post, but not the first, therefore we have to get that info
+			$god = allowed_to_see(get_post_author($bbc_options['cache']));
 			
-	}
-	if(in_array($board, $disabled_boards)){ // check if board is in disabled_boards (settings in acp)
-		$god = true;
-	}
-	if($god or user_has_liked_post($topic_id)){
-		$message = preg_replace('/\[hide\]/', $settings['hidemod_sa2'], $message);
-		$message = preg_replace('/\[\/hide\]/', !empty($settings['hidemod_sa3']) ? $settings['hidemod_sa3'] : '', $message);
-	}else{
-		// not allowed hide
-		$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
-	}
-	if($god or user_has_replied_to_topic($topic)){
-		$message = preg_replace('/\[hide-reply\]/', $settings['hidemod_sb2'], $message);
-		$message = preg_replace('/\[\/hide-reply\]/', !empty($settings['hidemod_sb3']) ? $settings['hidemod_sb3'] : '', $message);
-	}else{
-		$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
-	}
+		}
+		if(in_array($board, $disabled_boards)){ // check if board is in disabled_boards (settings in acp)
+			$god = true;
+		}
+		if($god or user_has_liked_post($bbc_options['cache'])){
+			$message = preg_replace('/\[hide\]/', $settings['hidemod_sa2'], $message);
 
+			$message = preg_replace('/\[\/hide\]/', !empty($settings['hidemod_sa3']) ? $settings['hidemod_sa3'] : '', $message);
+		}else{
+			// not allowed hide
+			$message = preg_replace($pattern_search_hide, $settings['hidemod_sa1'], $message);
+		}
+
+		if($god or user_has_replied_to_topic($topic)){
+			$message = preg_replace('/\[hide-reply\]/', $settings['hidemod_sb2'], $message);
+			$message = preg_replace('/\[\/hide-reply\]/', !empty($settings['hidemod_sb3']) ? $settings['hidemod_sb3'] : '', $message);
+		}else{
+			$message = preg_replace($pattern_search_hide_reply, $settings['hidemod_sb1'],$message);
+		}
+	}
 	
 
 }
