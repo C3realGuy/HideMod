5754f8fa49d775e12521d1a2e163b3e13ad9a2bb
C3realGuy <c3rgey@gmail.com>
2017-02-19 17:31:51 +0100

    We now add BBC Buttons to Editor and added backwards compatibility for validate-func

---
+++ lang/Hide-Editor.english.php
@@ -0,0 +1,4 @@
+<?php
+
+$txt['hide_editor_hide_desc'] = 'Hide';
+$txt['hide_editor_hide_reply_desc'] = 'Hide-reply';
---
+++ src/Hooks-Hide.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * [hide_bbc_buttons description]
+ * @hook bbc_buttons
+ * @param        [type]                  $bbc [description]
+ * @return       [type]                       [description]
+ */
+function hide_bbc_buttons(&$bbc) {
+    global $txt;
+    loadPluginLanguage('CerealGuy:Hide', 'lang/Hide-Editor');
+
+    array_splice(
+        $bbc[0],
+        5,
+        0,
+        [
+            [
+                'image' => 'http://localhost:8080/plugins/hide2/assets/bbc_hide.png',
+            	'code' => 'hide',
+            	'before' => '[hide]',
+                'after' => '[/hide]',
+            	'description' => $txt['hide_editor_hide_desc'],
+
+            ],
+            [
+                'image' => 'http://localhost:8080/plugins/hide2/assets/bbc_hide_reply.png',
+    			'code' => 'hide-reply',
+    			'before' => '[hide-reply]',
+                'after' => '[/hide-reply]',
+    			'description' => $txt['hide_editor_hide_reply_desc'],
+            ],
+            [],
+        ]
+    );
+    file_put_contents('/tmp/test', var_export($bbc, true));
+
+}
